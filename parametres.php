75bfaed79cc773e9ef74364c26844f13a834bbb8
Sana83 <83754212+Sana83@users.noreply.github.com>
2021-10-20 10:51:13 +0200

    Partie 4.3 : Operations sur les classes Controller et Model

--- parametres.php
+++ parametres.php
@@ -6,3 +6,8 @@
  * and open the template in the editor.
  */
 
+define('DATABASE_URL', "localhost");
+define('DATABASE_USER', "Tangui");
+define('DATABASE_PWD', "btssio2021.");
+define('DATABASE_NAME',"clicommvc");
+define('CNSTRING', "mysql:host=". DATABASE_URL.";dbname=". DATABASE_NAME.";charset=UTF8");
--- src/Model/GestionClientModel.php
+++ src/Model/GestionClientModel.php
@@ -1,7 +1,18 @@
 <?php
-namespace APP\Modele;
+namespace APP\Model;
+
+use \PDO;
+use APP\Entity\Client;
+use Tools\Connexion;
 
 class GestionClientModel {
-    
+    public function find($id) {
+        $unObjetPdo = Connexion::getConnexion();
+        $sql = "select * from CLIENT where id=:id";
+        $ligne = $unObjetPdo->prepare($sql);
+        $ligne->bindValue(':id', $id, PDO::PARAM_INT);
+        $ligne->execute();
+        return $ligne->fetchObject(Client::class);
+    }
 }
 
