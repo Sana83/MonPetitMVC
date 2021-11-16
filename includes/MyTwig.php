7d9accb59437ead2048a3e1716330c9c115a3ba1
Sana83 <83754212+Sana83@users.noreply.github.com>
2021-11-16 09:22:57 +0100

    Partie 2 : Travail à faire

--- includes/MyTwig.php
+++ includes/MyTwig.php
@@ -1,15 +1,21 @@
 <?php
 namespace Tools;
 
-abstract class Mytwig{
+abstract class MyTwig {
     
     private static function getLoader() {
         
-        $loader = new \Twig_Loader_Filesystem(PATH_VIEW); //Dossier contenant les templates
-        //pas de cache en mode debug
+        $loader = new \Twig_Loader_Filesystem(PATH_VIEW); // Dossier contenant les templates
+        // pas de cache en mode debug
         return new \Twig_Environment($loader, array(
             'cache' => false
         ));
     }
     
+    public static function afficheVue($vue, $params) {
+        $twig = self::getLoader();
+        
+        $template = $twig->loadTemplate($vue);
+        echo $template->render($params);
+    }
 }
--- src/Controller/GestionClientController.php
+++ src/Controller/GestionClientController.php
@@ -1,21 +1,14 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace APP\Controller;
 
 use APP\Model\GestionClientModel;
+use Tools\MyTwig;
 use ReflectionClass;
-use Exception;
-use Tools\Mytwig;
+use \Exception;
 
 class GestionClientController {
     
-    public function chercheUn($params){
+    public function chercheUn($params) {
         //appel de la méthode find($id) de la classe Model adequate
         $modele = new GestionClientModel();
         $id = filter_var(intval($params["id"]), FILTER_VALIDATE_INT);
@@ -25,18 +18,18 @@ class GestionClientController {
             $vue = str_replace('Controller', 'View', $r->getShortName()) . "/unClient.html.twig";
             MyTwig::afficheVue($vue, array('unClient' => $unClient));
         } else {
-            throw new Exception("Client" . $id . " inconnu");
+            throw new Exception("Client " . $id . " inconnu");
         }
     }
-    
-     public function chercheTous(){
+    public function chercheTous() {
+        //appel de la méthode findAll() de la classe Model adequate
         $modele = new GestionClientModel();
         $clients = $modele->findAll();
         if ($clients) {
             $r = new ReflectionClass($this);
             include_once PATH_VIEW . str_replace('Controller', 'View', $r->getShortName()) . "/plusieursClients.php";
         } else {
-            throw new Exception("Aucun client à afficher");
+            throw new Exception("Aucun Client à afficher");
         }
-     }
+    }
 }
